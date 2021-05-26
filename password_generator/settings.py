d2d6214a6ad795c84e341631f43d7c817fdfe349
Alessandro <alessandro.masella@webranking.it>
2021-05-26 17:48:11 +0200

    save

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_b9x2il@-xiqtuzauz!c7wyu3#!w$1m%ik2$23*0r!s(i6@3m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
